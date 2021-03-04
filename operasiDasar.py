ad7f40df170a6739fa66225fb4ffc94f87f54c8f
triputra3 <80019737+triputra3@users.noreply.github.com>
2021-03-04 10:54:32 +0700

    Update operasiDasar.py
    Memasukkan operasi penjumlahan

--- operasiDasar.py
+++ operasiDasar.py
@@ -1,1 +1,9 @@
+# Masukkan angka
+no1 = input('Masukkan angka pertama: ')
+no2 = input('Masukkan angka kedua: ')
 
+# Operasi dasar terjadi
+sum = float(no1) + float(no2)
+
+# Output
+print('Hasil penjumlahan {0} dan {1} adalah {2}'.format(no1, no2, sum))
