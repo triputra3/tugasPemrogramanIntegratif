58e6030ddcbb2b8984a37033d713587c2cc759ca
triputra3 <80019737+triputra3@users.noreply.github.com>
2021-03-04 10:55:14 +0700

    Update operasiDasar.py
    Mengubah operasi menjadi pengurangan

--- operasiDasar.py
+++ operasiDasar.py
@@ -3,7 +3,7 @@ no1 = input('Masukkan angka pertama: ')
 no2 = input('Masukkan angka kedua: ')
 
 # Operasi dasar terjadi
-sum = float(no1) + float(no2)
+sum = float(no1) - float(no2)
 
 # Output
-print('Hasil penjumlahan {0} dan {1} adalah {2}'.format(no1, no2, sum))
+print('Hasil pengurangan {0} dan {1} adalah {2}'.format(no1, no2, sum))
