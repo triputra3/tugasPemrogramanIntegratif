9213d25d6aebd5c8983dd0c6aed1e79dc470c0c8
triputra3 <80019737+triputra3@users.noreply.github.com>
2021-03-04 10:57:35 +0700

    Update operasiDasar.py
    Mengubah operasi menjadi pembagian

--- operasiDasar.py
+++ operasiDasar.py
@@ -3,7 +3,7 @@ no1 = float(input('Masukkan angka pertama: '))
 no2 = float(input('Masukkan angka kedua: '))
 
 # Operasi dasar terjadi
-kal = no1 * no2;
+bag = no1 / no2;
 
 # Output
-print('Hasil perkalian dari angka yang diberikan adalah:',kal)
+print('Hasil pembagian dari angka yang diberikan adalah:',bag)
